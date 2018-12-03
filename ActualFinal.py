1069f4c5e1164299aebfc8e2d269fa7bfae421d9
Caleb Davis (CSDAVI8841) <CSDAVI8841@ung.edu>
2018-12-03 18:09:56 -0500

    Added while loop I added the while loop to make the game run.

--- ActualFinal.py
+++ ActualFinal.py
@@ -233,15 +233,35 @@ wordbank =
 'Worm', 
 'X-ray']
 
-word_bank = random.choice(wordbank)
+word = random.choice(wordbank)
 name = raw_input("What is your name? ")
 
 print "Hello, " + name, "Let's play hangman!"
 
 print "Start guessing..."
 
-word = word_bank
 
 guesses = ''
 
 turns = 10
+
+while turns > 0:         
+
+    failed = 0             
+   
+    for char in word:      
+
+        if char in guesses:    
+    
+            print char,    
+
+        else:
+    
+            print "_",     
+       
+            failed += 1    
+
+    if failed == 0:        
+        print "You won!"  
+
+        break              
